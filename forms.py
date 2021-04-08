ea3d5bfe35c39494dc3f08ebb5ec868c768c8b3d
Francois <FrancoisNelles@gmail.com>
2021-04-08 18:03:50 +0200

    Pairwise Comparison Page
    Editing of the page

--- forms.py
+++ forms.py
@@ -1,8 +1,13 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from wtforms import StringField, PasswordField, SubmitField, BooleanField, FileField
 
 class LoginForm(FlaskForm):
     username = StringField('Username',validators=[])
     password = PasswordField('Password', validators=[])
     remember = BooleanField('Remember Me')
     submit = SubmitField('Login')
+
+class PCForm(FlaskForm):
+    reported_file = FileField('Reported Document',validators=[])
+    suspected_files = FileField('Suspected Document', validators=[])
+    submit = SubmitField('Upload')
